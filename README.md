6396d5c382df77f5c3725d8ac328989bbbff6ba3
vivekhimalayas <rockufriends@gmail.com>
2019-08-25 10:42:54 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# node
+# sample-nodejs-app
+Sample app for Dockerizing from the blog post "Deploying a Node.js application to Amazon Web Services using Docker".
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-var express = require('express');
-var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello World!, I an back');
-});
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+const express = require('express')
+const app = express()
+app.get('/', (req, res) => {
+    res.send('Hello world from a Node.js app!')
+})
+app.listen(3000, () => {
+    console.log('Server is up on 3000')
+})
